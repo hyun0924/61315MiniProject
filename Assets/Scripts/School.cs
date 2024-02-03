56d1a3f3df52c996eec331762a3514462ed49eaf
skku-sleepwalk <lucas7211@g.skku.edu>
2024-02-03 12:15:26 +0900

    boss

--- Assets/Scripts/School.cs
+++ Assets/Scripts/School.cs
@@ -8,13 +8,17 @@ public class School : MonoBehaviour
     protected double HP;
     public double MaxHP;
     protected static School instance = null;
+    public static bool levelUp;
+    public static int stack;
     void Awake()
     {
         if (null == instance)
         {
             //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
             instance = this;
-
+            instance.MaxHP = 100d;
+            School.levelUp = false;
+            School.stack = 0;
             //씬 전환이 되더라도 파괴되지 않게 한다.
             //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
             //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
@@ -38,8 +42,22 @@ public class School : MonoBehaviour
     }
     public void ReGen()
     {
-        MaxHP *= 1.1;
-        HP = MaxHP;
+        School.stack++;
+        if (levelUp)
+        {
+            MaxHP *= 1.1;
+            levelUp = false;
+            School.stack = 0;
+        }
+        else if (School.stack >= 19) levelUp = true;
+       
+       
+        //보스 체크
+        
+        
+        HP = !levelUp?MaxHP:MaxHP*5;
+        
+        
     }
     public void GetAttack(int dmg)
     {
@@ -48,6 +66,13 @@ public class School : MonoBehaviour
         {
             //대충 죽는 처리
             Money.IncreaseMoney(Random.Range(2, 4));
+            if (levelUp)
+            {
+                Money.IncreaseMoney(Random.Range(2, 4));
+                Money.IncreaseMoney(Random.Range(2, 4));
+                Money.IncreaseMoney(Random.Range(2, 4));
+                Money.IncreaseMoney(Random.Range(2, 4));
+            }
         }
         //대충 애니메이션 
     }
