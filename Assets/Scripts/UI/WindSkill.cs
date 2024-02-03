503b58e3aafab6a0dd9ecc48066c305e1b8942bd
skku-sleepwalk <lucas7211@g.skku.edu>
2024-02-04 00:01:59 +0900

    wind_skill_add

--- Assets/Scripts/UI/WindSkill.cs
+++ Assets/Scripts/UI/WindSkill.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class WindSkill : MonoBehaviour
 {
+    public GameObject windPrefab;
     private void OnMouseDown()
     {
         if (BackgroundTouch.skillCount >= 50)
         {
             School.getInstance().GetAttack( PlayerStat.atk * 5);
             BackgroundTouch.skillCount = 0;
+            Instantiate(windPrefab);
         }
         //치명타 추가
     }
