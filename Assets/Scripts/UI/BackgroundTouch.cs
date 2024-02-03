0c426b65fbd8dbdeb16ae728353b9aa4616b5837
skku-sleepwalk <lucas7211@g.skku.edu>
2024-02-03 22:17:30 +0900

    indent_Erase

--- Assets/Scripts/UI/BackgroundTouch.cs
+++ Assets/Scripts/UI/BackgroundTouch.cs
@@ -9,6 +9,5 @@ public class BackgroundTouch : MonoBehaviour
         bool crit = Random.Range(0, 50) == 0;
         School.getInstance().GetAttack(crit ? PlayerStat.atk*10 : PlayerStat.atk);
         //치명타 추가
-        
     }
 }
