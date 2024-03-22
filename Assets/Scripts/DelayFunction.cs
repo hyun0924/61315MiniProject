2242877b8790ce2f215fb761dec089b61fa5c2ed
Hyun <jaehyun6687@naver.com>
2024-03-22 15:10:15 +0900

    Update ClearPanel
    보스 격파 시 2초간 터치 안되도록 수정

---
+++ Assets/Scripts/DelayFunction.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayFunction : MonoBehaviour
+{
+    [SerializeField] Action action;
+
+    private void OnEnable()
+    {
+
+    }
+}
